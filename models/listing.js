b36c1b2e4550e2551451185125848c5cc4095b45
lalitsain03 <sainlalit072@gmail.com>
2025-08-02 11:05:38 +0530

    change one Category

--- models/listing.js
+++ models/listing.js
@@ -42,7 +42,7 @@ const listingSchema = new Schema({
     },
     category: {
         type: String,
-        enum: ["Trending", "Rooms", "Iconic Cities", "Mountains", "Castles", "Amazing Pool", "Camping", "Farming", "Arctic", "Domes", "Boats"]
+        enum: ["Trending", "Rooms", "Iconic Cities", "Mountains", "Castles", "Amazing Pools", "Camping", "Farming", "Arctic", "Domes", "Boats"]
     }
 })
 
